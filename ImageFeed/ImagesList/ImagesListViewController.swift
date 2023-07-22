803671f36c6e96bb4c400c7bee4a57471e260408
KaMaDi <artkamadi@gmail.com>
2023-07-22 09:31:44 +0300

    add UiTabBar+image

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -16,13 +16,14 @@ final class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+         var preferredStatusBarStyle: UIStatusBarStyle {
+            return .lightContent
+        }
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
     
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
+    
 }
 
 extension ImagesListViewController: UITableViewDataSource {
