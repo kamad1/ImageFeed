03874a583f7fbc1e8291e72ba7d53bf5483f6676
KaMaDi <artkamadi@gmail.com>
2023-07-22 21:17:59 +0300

    все сделано. Сравнил с Авторским решением поведение скролинга идентично. но как то не эстетически смотрится

--- ImageFeed/SingleImageViewController/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -1,9 +1,3 @@
-//
-//  SingleImageViewController.swift
-//  ImageFeed
-//
-//  Created by Jedi on 22.07.2023.
-//
 
 import UIKit
 
@@ -12,6 +6,7 @@ final class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
@@ -21,12 +16,45 @@ final class SingleImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        rescaleAndCenterImageInScrollView(image: image)
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
     }
     
     @IBAction private func didTapBackButton() {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        let share = UIActivityViewController(
+            activityItems: [image as Any],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
 }
 
 
@@ -47,19 +75,19 @@ final class SingleImageViewController: UIViewController {
 /*Итак, если нам нужно подменять изображение уже после viewDidLoad, мы можем реализовать обработчик didSet для image следующим образом:
  
  final class SingleImageViewController: UIViewController {
-     var image: UIImage! {
-         didSet {
-             guard isViewLoaded else { return } // 1
-             imageView.image = image // 2
-         }
-     }
-     
-     @IBOutlet var imageView: UIImageView!
-     
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         imageView.image = image
-     }
+ var image: UIImage! {
+ didSet {
+ guard isViewLoaded else { return } // 1
+ imageView.image = image // 2
+ }
+ }
+ 
+ @IBOutlet var imageView: UIImageView!
+ 
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ imageView.image = image
+ }
  }
  
  
