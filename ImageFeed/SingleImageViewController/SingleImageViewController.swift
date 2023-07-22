6d559fbb64aee79d1103667ba25adff5282d160d
KaMaDi <artkamadi@gmail.com>
2023-07-22 11:08:57 +0300

    add single image view controller

---
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Jedi on 22.07.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
