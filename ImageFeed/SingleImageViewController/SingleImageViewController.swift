6204c96adeb7dfb9385b2892638526f4cca78484
KaMaDi <artkamadi@gmail.com>
2023-07-22 16:20:38 +0300

    change ux

--- ImageFeed/SingleImageViewController/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -8,6 +8,59 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Итак, если нам нужно подменять изображение уже после viewDidLoad, мы можем реализовать обработчик didSet для image следующим образом:
+ 
+ final class SingleImageViewController: UIViewController {
+     var image: UIImage! {
+         didSet {
+             guard isViewLoaded else { return } // 1
+             imageView.image = image // 2
+         }
+     }
+     
+     @IBOutlet var imageView: UIImageView!
+     
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         imageView.image = image
+     }
+ }
+ 
+ 
+ Прокомментируем то, что мы сделали:
+ Сначала мы проверяем, было ли ранее загружено view. Это очень важная проверка, необходимая, чтобы не закрэшиться, если view ещё не было загружено (и, соответственно, аутлет ещё не инициализирован). Именно эта проверка не даст нам закрэшиться из prepareForSegue.
+ В эту точку мы не должны попадать из prepareForSegue. Мы можем попасть в неё, например, если был показан SingleImageViewController, а указатель на него был запомнен извне. Далее  — извне (например, по свайпу) в него проставляется новое изображение.
+ 
+ 
+ */
+
