c38a245d907792f9bef539054c96112ea59e6390
KaMaDi <artkamadi@gmail.com>
2023-08-01 22:24:43 +0300

    no message

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -20,7 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+<<<<<<< Updated upstream
         
+=======
+       
+>>>>>>> Stashed changes
     }
 
 
