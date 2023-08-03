c2164e0d324e5139151f80e65f88784958ba5b28
KaMaDi <artkamadi@gmail.com>
2023-08-03 23:22:17 +0300

    верстка экрана входа добавил лого

---
+++ ImageFeed/Service/Constants.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+let AccessKey = "<ваш Access Key"
+let SecretKey = "<ваш Secret Key"
+let RedirectURI = "<ваш Redirect URI>"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
