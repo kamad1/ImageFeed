1d5b8ac68247b4588201235752623e96b8b5fe12
KaMaDi <artkamadi@gmail.com>
2023-08-05 17:30:33 +0300

    WebViewViewController. добавил экран авторизации может быть несколько фото кнопки назад учитывая прблеммы коллег что не выгружаются добавил и те что удалил из проекта

--- ImageFeed/Auth/WebViewController.swift
+++ ImageFeed/Auth/WebViewController.swift
@@ -1,5 +1,26 @@
 import UIKit
+import WebKit
 
 final class WebViewViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
     
+    @IBAction private func didTapBackButton(_ sender: Any?) { }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+                urlComponents.queryItems = [
+                    URLQueryItem(name: "client_id", value: AccessKey),
+                    URLQueryItem(name: "redirect_uri", value: RedirectURI),
+                    URLQueryItem(name: "response_type", value: "code"),
+                    URLQueryItem(name: "scope", value: AccessScope)
+                ]
+                
+                let url = urlComponents.url!
+                let request = URLRequest(url: url)
+                
+                webView.load(request)
+       
+    }
 }
--- ImageFeed/Service/Constants.swift
+++ ImageFeed/Service/Constants.swift
@@ -6,3 +6,4 @@ let SecretKey = "<ваш Secret Key"
 let RedirectURI = "<ваш Redirect URI>"
 let AccessScope = "public+read_user+write_likes"
 let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
