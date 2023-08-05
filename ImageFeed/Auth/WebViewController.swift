1ce8c1c85e3a76022fbccceff2f74117ae3a2cb8
KaMaDi <artkamadi@gmail.com>
2023-08-05 18:20:44 +0300

    реализация делегата

--- ImageFeed/Auth/WebViewController.swift
+++ ImageFeed/Auth/WebViewController.swift
@@ -1,13 +1,19 @@
 import UIKit
 import WebKit
 
+
+
 final class WebViewViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     
-    @IBAction private func didTapBackButton(_ sender: Any?) { }
+
+    
+    weak var delegate: WebViewViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webView.navigationDelegate = self
         
         var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
                 urlComponents.queryItems = [
@@ -21,6 +27,40 @@ final class WebViewViewController: UIViewController {
                 let request = URLRequest(url: url)
                 
                 webView.load(request)
-       
+    }
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+        delegate?.webViewViewControllerDidCancel(self)
     }
 }
+
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(
+            _ webView: WKWebView,
+            decidePolicyFor navigationAction: WKNavigationAction,
+            decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+        ) {
+            if let code = code(from: navigationAction) {
+                delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+                decisionHandler(.cancel)
+            } else {
+                decisionHandler(.allow)
+            }
+        }
+        
+        private func code(from navigationAction: WKNavigationAction) -> String? {
+            if let url = navigationAction.request.url,
+               let urlComponents = URLComponents(string: url.absoluteString),
+               urlComponents.path == "/oauth/authorize/native",
+               let items = urlComponents.queryItems,
+               let codeItem = items.first(where: { $0.name == "code" })
+            {
+                return codeItem.value
+            } else {
+                return nil
+            }
+        }
+}
+
+//extension WebViewViewController: UIViewController {
+//
+//}
