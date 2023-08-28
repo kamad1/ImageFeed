6f94a0c0ddf6f653b7dabf9241d3c4bce04c3595
KaMaDi <artkamadi@gmail.com>
2023-08-29 00:22:14 +0300

    no message

--- ImageFeed/TabBarController/TabBarController.swift
+++ ImageFeed/TabBarController/TabBarController.swift
@@ -6,6 +6,7 @@ final class TabBarController: UITabBarController {
         super.awakeFromNib()
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
         let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+        view.backgroundColor = #colorLiteral(red: 0.1019607843, green: 0.1058823529, blue: 0.1333333333, alpha: 1)
             let profileViewController = ProfileViewController()
             profileViewController.tabBarItem = UITabBarItem(
             title: nil,
