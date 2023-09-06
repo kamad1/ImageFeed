346c8262c28591d1711b9802e17ea3365d1ed920
KaMaDi <artkamadi@gmail.com>
2023-09-07 00:05:00 +0300

    анимация  при загрузке профиля

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -6,23 +6,25 @@ import WebKit
 final class ProfileViewController: UIViewController {
     
     private struct Keys {
-             static let main = "Main"
-             static let logoutImageName = "logout_button"
-             static let systemLogoutImageName = "ipad.and.arrow.forward"
-             static let logOutActionName = "logOut"
-             static let systemAvatarImageName = "person.crop.circle.fill"
-             static let avatarPlaceholderImageName = "avatar_placeholder"
-             static let authViewControllerName = "AuthViewController"
-         }
+        static let main = "Main"
+        static let logoutImageName = "logout_button"
+        static let systemLogoutImageName = "ipad.and.arrow.forward"
+        static let logOutActionName = "logOut"
+        static let systemAvatarImageName = "person.crop.circle.fill"
+        static let avatarPlaceholderImageName = "avatar_placeholder"
+        static let authViewControllerName = "AuthViewController"
+    }
     
     private var profileImageServiceObserver: NSObjectProtocol?
     private let profileService = ProfileService.shared
     private let profileImageService = ProfileImageService.shared
     private var label: UILabel?
+    private let translucentGradient = TranslucentGradient()
+    private var animationLayers = Set<CALayer>()
     
     private var avatarImageView: UIImageView = {
-//        let imageView = UIImageView()
-//        imageView.image = UIImage(named: "avatar")
+        //        let imageView = UIImageView()
+        //        imageView.image = UIImage(named: "avatar")
         let imageView = UIImageView(image: UIImage(systemName: Keys.systemAvatarImageName))
         imageView.tintColor = .gray
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -32,7 +34,7 @@ final class ProfileViewController: UIViewController {
     
     private var nameLabel: UILabel = {
         let labelname = UILabel()
-//        labelname.text = "Екатерина Новикова"
+        //        labelname.text = "Екатерина Новикова"
         labelname.textAlignment = .left
         labelname.textColor = .white
         labelname.font = .systemFont(ofSize: 23, weight: UIFont.Weight.bold)
@@ -43,7 +45,7 @@ final class ProfileViewController: UIViewController {
     
     private  var loginNameLabel: UILabel = {
         let loginLabel = UILabel()
-//        loginLabel.text = "@ekaterina_nov"
+        //        loginLabel.text = "@ekaterina_nov"
         loginLabel.textAlignment = .left
         loginLabel.textColor = .lightGray
         loginLabel.font = .systemFont(ofSize: 13)
@@ -54,7 +56,7 @@ final class ProfileViewController: UIViewController {
     
     private var descriptionLabel: UILabel = {
         let descriptionText = UILabel()
-//        descriptionText.text = "Hello, World!"
+        //        descriptionText.text = "Hello, World!"
         descriptionText.textColor = .white
         descriptionText.font = .systemFont(ofSize: 13)
         descriptionText.textAlignment = .left
@@ -63,52 +65,52 @@ final class ProfileViewController: UIViewController {
         return descriptionText
     }()
     
-//    lazy var logoutButton: UIButton = {
-//
-//        let button = UIButton.systemButton(
-//            with: UIImage(named: "logout_button") ?? UIImage(),
-//            target: self,
-//            action: #selector(Self.didTapLogoutButton)
-//        )
-//
-//        button.tintColor = #colorLiteral(red: 0.9607843137, green: 0.4196078431, blue: 0.4235294118, alpha: 1)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        if #available(iOS 14.0, *) {
-//
-//                      //TODO: Выход из профиля
-//                    let logOutAction = UIAction(title: "logOut") { (ACTION) in
-//                        ProfileViewController.logOut()
-//                    }
-//                    button.addAction(logOutAction, for: .touchUpInside)
-//                } else {
-//                   button.addTarget(ProfileViewController.self,
-//                                    action: #selector(didTapLogoutButton),
-//                                    for: .touchUpInside)
-//               }
-//        return button
-//    }()
+    //    lazy var logoutButton: UIButton = {
+    //
+    //        let button = UIButton.systemButton(
+    //            with: UIImage(named: "logout_button") ?? UIImage(),
+    //            target: self,
+    //            action: #selector(Self.didTapLogoutButton)
+    //        )
+    //
+    //        button.tintColor = #colorLiteral(red: 0.9607843137, green: 0.4196078431, blue: 0.4235294118, alpha: 1)
+    //        button.translatesAutoresizingMaskIntoConstraints = false
+    //        if #available(iOS 14.0, *) {
+    //
+    //                      //TODO: Выход из профиля
+    //                    let logOutAction = UIAction(title: "logOut") { (ACTION) in
+    //                        ProfileViewController.logOut()
+    //                    }
+    //                    button.addAction(logOutAction, for: .touchUpInside)
+    //                } else {
+    //                   button.addTarget(ProfileViewController.self,
+    //                                    action: #selector(didTapLogoutButton),
+    //                                    for: .touchUpInside)
+    //               }
+    //        return button
+    //    }()
     
     lazy var logoutButton: UIButton = {
         
         let image = UIImage(named: Keys.logoutImageName) ?? UIImage(systemName: Keys.systemLogoutImageName)!
         
-   
+        
         let button = UIButton(type: .custom)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(image, for: .normal)
         button.tintColor = #colorLiteral(red: 0.9607843137, green: 0.4196078431, blue: 0.4235294118, alpha: 1)
         if #available(iOS 14.0, *) {
-
-                      //TODO: Выход из профиля
+            
+            //TODO: Выход из профиля
             let logOutAction = UIAction(title: Keys.logOutActionName) { (ACTION) in
                 ProfileViewController.logOut()
-                    }
-                    button.addAction(logOutAction, for: .touchUpInside)
-                } else {
-                   button.addTarget(ProfileViewController.self,
-                                    action: #selector(didTapLogoutButton),
-                                    for: .touchUpInside)
-               }
+            }
+            button.addAction(logOutAction, for: .touchUpInside)
+        } else {
+            button.addTarget(ProfileViewController.self,
+                             action: #selector(didTapLogoutButton),
+                             for: .touchUpInside)
+        }
         return button
     }()
     
@@ -141,40 +143,41 @@ final class ProfileViewController: UIViewController {
         
         updateProfileDetails(profile: profileService.profile)
         
+        addGradients()
     }
     
-//     func updateProfileDetails(profile: Profile?) {
-//        guard let profile = profile else { return }
-//        nameLabel.text = profile.name
-//        loginNameLabel.text = profile.loginName
-//        descriptionLabel.text = profile.bio
-//
-//        profileImageServiceObserver = NotificationCenter.default.addObserver(
-//            forName: ProfileImageService.DidChangeNotification,
-//            object: nil,
-//            queue: .main
-//        ) { [weak self] _ in
-//            guard let self = self else { return }
-//            self.updateAvatar()
-//        }
-//        updateAvatar()
-//    }
+    //     func updateProfileDetails(profile: Profile?) {
+    //        guard let profile = profile else { return }
+    //        nameLabel.text = profile.name
+    //        loginNameLabel.text = profile.loginName
+    //        descriptionLabel.text = profile.bio
+    //
+    //        profileImageServiceObserver = NotificationCenter.default.addObserver(
+    //            forName: ProfileImageService.DidChangeNotification,
+    //            object: nil,
+    //            queue: .main
+    //        ) { [weak self] _ in
+    //            guard let self = self else { return }
+    //            self.updateAvatar()
+    //        }
+    //        updateAvatar()
+    //    }
     func updateProfileDetails(profile: Profile?) {
-            guard let profile = profile else { return }
-            nameLabel.text = profile.name
-            loginNameLabel.text = profile.loginName
-            descriptionLabel.text = profile.bio
-            
-            profileImageServiceObserver = NotificationCenter.default.addObserver(
-                forName: ProfileImageService.DidChangeNotification,
-                object: nil,
-                queue: .main
-            ) { [weak self] _ in
-                guard let self = self else { return }
-                self.updateAvatar()
-            }
-            updateAvatar()
+        guard let profile = profile else { return }
+        nameLabel.text = profile.name
+        loginNameLabel.text = profile.loginName
+        descriptionLabel.text = profile.bio
+        
+        profileImageServiceObserver = NotificationCenter.default.addObserver(
+            forName: ProfileImageService.DidChangeNotification,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            guard let self = self else { return }
+            self.updateAvatar()
         }
+        updateAvatar()
+    }
     
     func avatarImageSetup() {
         avatarImageView.layer.masksToBounds = true
@@ -213,25 +216,19 @@ final class ProfileViewController: UIViewController {
     }
     
     @objc
-     func didTapLogoutButton() {
+    func didTapLogoutButton() {
         ProfileViewController.logOut()
     }
     
 }
 
- private extension ProfileViewController {
+private extension ProfileViewController {
     func updateAvatar() {
         guard
             let avatarURL = profileImageService.avatarURL,
             let url = URL(string: avatarURL)
         else { return }
         
-        
-//        let cache = ImageCache.default
-//        cache.clearMemoryCache()
-//        cache.clearDiskCache()
-        
-//        let avatarPlaceholderImage = UIImage(named: "avatar_placeholder")
         let avatarPlaceholderImage = UIImage(named: Keys.avatarPlaceholderImageName)
         
         let processor = RoundCornerImageProcessor(cornerRadius: 61)
@@ -240,22 +237,67 @@ final class ProfileViewController: UIViewController {
             with: url,
             placeholder: avatarPlaceholderImage,
             options: [.processor(processor)]
-        )
+        ){ [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success:
+                self.removeGradients()
+            case .failure:
+                print("!ОШИБКА загрузки аватара")
+            }
+        }
     }
     
-     static func logOut() {
-              OAuth2TokenStorage().token = nil
-              WebViewViewController.cleanCookies()
-
-              guard let window = UIApplication.shared.windows.first else {
-                  assertionFailure("Invalid Configuration")
-                  return
-              }
-
-//              let authViewController = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "AuthViewController")
-         let authViewController = UIStoryboard(name: Keys.main, bundle: .main).instantiateViewController(withIdentifier: Keys.authViewControllerName)
-              window.rootViewController = authViewController
-          }
-     
+    func addGradients() {
+        let avatarGradient = translucentGradient.getGradient(
+            size: CGSize(
+                width: 70,
+                height: 70
+            ),
+            cornerRadius: avatarImageView.layer.cornerRadius)
+        avatarImageView.layer.addSublayer(avatarGradient)
+        animationLayers.insert(avatarGradient)
+        
+        let nameLabelGradient = translucentGradient.getGradient(size: CGSize(
+            width: 223,//nameLabel.bounds.width,
+            height: 27//nameLabel.bounds.height
+        ))
+        nameLabel.layer.addSublayer(nameLabelGradient)
+        animationLayers.insert(nameLabelGradient)
+        
+        let descriptionLabelGradient = translucentGradient.getGradient(size: CGSize(
+            width: 223,//descriptionLabel.bounds.width,
+            height: 20//descriptionLabel.bounds.height
+        ))
+        descriptionLabel.layer.addSublayer(descriptionLabelGradient)
+        animationLayers.insert(descriptionLabelGradient)
+        
+        let loginLabelGradient = translucentGradient.getGradient(size: CGSize(
+            width: 223,//loginNameLabel.bounds.width,
+            height: 20
+        ))
+        loginNameLabel.layer.addSublayer(loginLabelGradient)
+        animationLayers.insert(loginLabelGradient)
+    }
+    
+    func removeGradients() {
+        for gradient in animationLayers {
+            gradient.removeFromSuperlayer()
+        }
+    }
+    
+    static func logOut() {
+        OAuth2TokenStorage().token = nil
+        WebViewViewController.cleanCookies()
+        
+        guard let window = UIApplication.shared.windows.first else {
+            assertionFailure("Invalid Configuration")
+            return
+        }
+        
+        let authViewController = UIStoryboard(name: Keys.main, bundle: .main).instantiateViewController(withIdentifier: Keys.authViewControllerName)
+        window.rootViewController = authViewController
+    }
+    
 }
 
