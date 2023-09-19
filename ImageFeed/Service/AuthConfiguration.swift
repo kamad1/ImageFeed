0df89794158ee0d6ec2cfc6d31338a6fa0578d7f
KaMaDi <artkamadi@gmail.com>
2023-09-20 01:21:43 +0300

    no message

--- ImageFeed/Service/AuthConfiguration.swift
+++ ImageFeed/Service/AuthConfiguration.swift
@@ -1,10 +1,7 @@
 
 import Foundation
 
-//let AccessKey = "h6OHD3P5qMrGGmFzS6GRBr6xvJcKw4o7kXvstKJuKPk"
-//let SecretKey = "aRrzMAK-umgi5Hh0UcKJ-Ne0NWZ-i7sbDCjrikw1Nn0"
-//let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
-//let AccessScope = "public+read_user+write_likes"
+
 let DefaultBaseURL = URL(string: "https://api.unsplash.com")
 let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 
